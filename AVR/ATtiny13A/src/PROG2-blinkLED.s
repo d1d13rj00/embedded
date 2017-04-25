0cb35561d7200343d3aa173163178bee9d314e28
Didier JOOMUN <d.joomun@linkbynet.com>
2017-04-25 13:32:13 +0700

    Adding PROG2. Blinking LED with timers

---
+++ AVR/ATtiny13A/src/PROG2-blinkLED.s
@@ -0,0 +1,33 @@
+;-----------------------------------------------------------
+; Blink LED using counters
+; Author : Didier Michel JOOMUN
+;-----------------------------------------------------------
+
+; Defining ports addresses to ease coding
+
+.EQU PINB, 0x16	    ; PORT state (HIGH or LOW)
+.EQU DDRB, 0x17	    ; Port direction (INPUT or OUTPUT)
+.EQU PORTB,0x18	    ; PORT state (HIGH or LOW)
+
+; Tell the assembler that our code will execute at address 0x0 in memory
+.org 0x0
+
+; Defining Interrup Vector table
+    rjmp RESET	    ; RESET interrupt handler - Execution starts here at boot
+
+RESET:
+    sbi DDRB, 0	    ; Set PIN0 to OUTPUT mode
+    sbi PORTB,0	    ; Set PIN0 to HIGH
+
+END:
+    rcall DELAY
+    sbi PINB0,0	    ; Triggers the PIN. Refer to section 10.2.2 in ATtiny13A datasheet.
+    rjmp END	    ; Loop endlessly :)
+
+DELAY:
+    ; We want our LED to toggle 2 times per second (1 toggle every 500ms)
+    ldi r16,0xFF    ; Initialize the counter with a big value
+DEC_LOOP:
+    dec r16
+
+    reti
